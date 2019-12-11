a0e35430443511923d17d65d718ce81ca8537b3c
fjij <30779570+fjij@users.noreply.github.com>
2019-12-11 14:05:04 -0500

    comments

--- main.cpp
+++ main.cpp
@@ -71,37 +71,38 @@ int main() {
 
 	// VBO
 	GLuint VBO;
-	glGenBuffers(1, &VBO);
-
-	// EBO
-	GLuint EBO;
-	glGenBuffers(1, &EBO);
-
-	// Load shaders
-	shader::Shader main_shader = shader::Shader("shader.vert", "shader.frag");
-
-	// Creating a VAO to store our state
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+	glGenBuffers(1, &VBO); // Generates 1 VBO and stores it's ID at (&VBO)[0]
 
 	// Bind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// this is the buffer we are gonna write to
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// EBO -- for indexed drawing.
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+
 	// Bind EBO (you need to bind vbo and ebo together)
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	// Creating a VAO to store our state
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 	// Linking vertex attributes
-	// position
+	// >> position
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0); // 6 stride (color in between)
-	// color
+	// >> color
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float),
 	(void*)(3*sizeof(float))); // 3 float offset
 	glEnableVertexAttribArray(1);
+
+
+	// Load shaders
+	shader::Shader main_shader = shader::Shader("shader.vert", "shader.frag");
+
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
